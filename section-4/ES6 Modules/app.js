4e67d535b953379f5163e8b45c7aaa65bf3a425b
vikasdev66 <vikasmishra.webdev@gmail.com>
2024-10-01 17:43:57 +0530

    import.meta

--- section-4/ES6 Modules/app.js
+++ section-4/ES6 Modules/app.js
@@ -1,2 +1,9 @@
 import { num } from "./math.js";
 console.log(num);
+
+//Accessing filename and dirname in ES6 Modules
+
+const { filename, dirname } = import.meta; // import.meta can be accessed from within the module
+
+console.log(filename);
+console.log(dirname);
